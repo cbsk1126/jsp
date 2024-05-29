2b56d2e9ee66bd85a6209ca564d346c72af1c063
HappyCheon <cbsk1126@gmail.com>
2024-05-30 07:36:44 +0900

    20240530_0736 스켈레톤

--- src/main/java/study2/StudyController.java
+++ src/main/java/study2/StudyController.java
@@ -28,6 +28,10 @@ import study2.pdstest.FileUpload2OkCommand;
 import study2.pdstest.FileUpload3OkCommand;
 import study2.pdstest.FileUpload4OkCommand;
 import study2.pdstest.JavaFileDownloadCommand;
+import study2.scrollPage.ScrollPageCommand;
+import study2.transaction.TransactionBankBookCommand;
+import study2.transaction.TransactionTest1Command;
+import study2.transaction.TransactionTest2Command;
 
 @SuppressWarnings("serial")
 @WebServlet("*.st")
@@ -173,6 +177,40 @@ public class StudyController extends HttpServlet {
 			command = new Calendar2Command();
 			command.execute(request, response);
 			viewPage += "/calendar/calendar2.jsp";
+		}
+		else if(com.equals("ScrollStudy")) {
+			viewPage += "/scrollPage/scrollStudy.jsp";
+		}
+		else if(com.equals("ScrollBasic")) {
+			command = new ScrollPageCommand();
+			command.execute(request, response);
+			viewPage += "/scrollPage/scrollBasic.jsp";
+		}
+		else if(com.equals("ScrollPage")) {
+			command = new ScrollPageCommand();
+			command.execute(request, response);
+			viewPage += "/scrollPage/scrollPage.jsp";
+		}
+		else if(com.equals("Transaction")) {
+			viewPage += "/transaction/transaction.jsp";
+		}
+		else if(com.equals("TransactionBankBook")) {
+			command = new TransactionBankBookCommand();
+			command.execute(request, response);
+			viewPage += "/transaction/transactionBankBook.jsp";
+		}
+		else if(com.equals("TransactionTest1")) {
+			command = new TransactionTest1Command();
+			command.execute(request, response);
+			viewPage = "/include/message.jsp";
+		}
+		else if(com.equals("TransactionTest2")) {
+			command = new TransactionTest2Command();
+			command.execute(request, response);
+			viewPage = "/include/message.jsp";
+		}
+		else if(com.equals("Translator")) {
+			viewPage += "/translator/translator.jsp";
 		}
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
--- src/main/webapp/include/nav.jsp
+++ src/main/webapp/include/nav.jsp
@@ -74,6 +74,11 @@
 				    <div class="dropdown-menu">
 				      <a class="dropdown-item" href="Calendar1.st">달력연습1</a>
 				      <a class="dropdown-item" href="Calendar2.st">달력연습2</a>
+				      <a class="dropdown-item" href="ScrollStudy.st">스크롤 연습</a>
+				      <a class="dropdown-item" href="ScrollBasic.st">무한 스크롤</a>
+				      <a class="dropdown-item" href="Transaction.st">트랜잭션연습</a>
+				      <%-- <a class="dropdown-item" href="${ctp}/translator">웹 번역</a> --%>
+				      <a class="dropdown-item" href="Translator.st">웹 번역</a>
 				    </div>
 				  </div>
 	      </li>
